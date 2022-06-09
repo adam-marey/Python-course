a611850939a10f07e7847867f9436a78289cd0af
Sulaiman Marey <adammarey51@gmail.com>
2022-06-08 23:04:28 -0700

    added hello world and basic math hw

---
+++ CHP1/hello-world-basic-math.py
@@ -0,0 +1,6 @@
+# Hello world and basic math
+
+name = input("Enter your name: ")
+salary = float(input("Enter your yearly income: "))
+monthlyIncome = salary / 12;
+print("Monthly income of", name, "is",monthlyIncome)
