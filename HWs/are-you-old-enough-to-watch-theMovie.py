a6609561555865a58af43ce61381e6c372eb4fd0
Sulaiman Marey <adammarey51@gmail.com>
2022-06-11 00:02:53 -0700

    fixed a bug

--- HWs/are-you-old-enough-to-watch-theMovie.py
+++ HWs/are-you-old-enough-to-watch-theMovie.py
@@ -1,4 +1,3 @@
-# In this assignment you will create a simple branching program.  the requirements are as follows:
 
 # John wishes to go to a movie with his two friends, Sam, and Bob.
 #  John is 15, Sam is 18, and Bob is 67.  The program will do the following:
@@ -19,18 +18,18 @@ print('-------people who will see the Movie-------')
 # program to check John age
 if person1_age < 18:
     print("John can not see the  movie")
-elif person1_age >= 18 or person1_age < 65:
+elif person1_age < 65:
     print("John will charged 15$.")
 else:
-    print("John will charged 5$.")
+    print("John will charged 5$ after a $10 discount has been applied.")
 
 # program to check Sam age
 if peron2_age < 18:
     print("Sam can not see the  movie")
-elif peron2_age >= 18 or peron2_age < 65:
+elif  peron2_age < 65:
     print("Sam will charged 15$.")
 else:
-    print("Sam will charged 5$.")
+    print("Sam will charged 5$ after a $10 discount has been applied.")
 
 # program to check Bob age
 if perosn3_age < 18:
