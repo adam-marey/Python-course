497be84bbd9e40aab9ce364ad866bdf9c5cc1bb8
Sulaiman Marey <adammarey51@gmail.com>
2022-07-16 23:23:22 -0700

    nameList file

---
+++ HWs/list_for_python (1).py
@@ -0,0 +1,8 @@
+NameList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', ]
+
+
+firstName = (NameList[18].capitalize() + NameList[20] + NameList[11] + NameList[0] + NameList[8] + NameList[12] + NameList[0] + NameList[13])
+
+lastName = (NameList[12].capitalize() + NameList[0] + NameList[17] + NameList[4] + NameList[24])
+print(firstName + " " + lastName)
